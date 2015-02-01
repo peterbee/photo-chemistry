f6f0ea99330e7a818ad26bbc8c1e7ac0524e41c2
Pete Parker <pparker@rabbleandrouser.com>
2015-02-01 11:29:44 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,96 @@
+var chemistry = angular.module('chemistry',[]);
+
+chemistry.controller('CompareCtrl', ['$scope', 'Photos', function($scope, Photos) {
+    var parents = Photos.data;
+    var scopeParents = [];
+
+    function showRandomPhotos() {
+        var scopePhotos = [];
+        var scopeParent = parents.slice();
+
+        if(parents.length < 2) {
+            alert('Not enough items to compare.');
+            return;
+        }
+
+        for(var i = 0; i < 2; i++) {
+            var randomParent = scopeParent.splice([Math.floor(Math.random() * scopeParent.length)],1)[0];
+            scopeParents[i] = randomParent;
+            var randomPhoto = randomParent.photos[Math.floor(Math.random() * randomParent.photos.length)];
+            scopePhotos.push(randomPhoto);
+        }
+
+        $scope.photo1 = scopePhotos[0];
+        $scope.photo2 = scopePhotos[1];
+    }
+
+    $scope.click = function click(id) {
+        var parent = scopeParents[id];
+        Photos.select(parent.id);
+
+        for(var i in scopeParents) {
+            var parent = scopeParents[i];
+            Photos.show(parent.id);
+            parent.ratio = Photos.getRatio(parent.id);
+        }
+
+        showRandomPhotos();
+
+        Photos.save();
+    }
+
+    // initialize view
+    showRandomPhotos();
+}]);
+
+chemistry.controller('ListCtrl', ['$scope', 'Photos', function($scope, Photos) {
+    $scope.parents = Photos.data;
+}]);
+
+chemistry.service('Photos', ['PhotoProvider', function(PhotoProvider) {
+    function Photos() {
+        this.data = PhotoProvider;
+        this.results = {};
+    };
+
+    Photos.prototype.show = function select(id1, id2) {
+        this.getResults(id1).exposures++;
+        this.getResults(id2).exposures++;
+    };
+
+    Photos.prototype.select = function select(id) {
+        this.getResults(id).clicks++;
+    };
+
+    Photos.prototype.getRatio = function select(id) {
+        var result = this.getResults(id);
+        return result.exposures ? result.clicks / result.exposures : 0;
+    };
+
+    Photos.prototype.getResults = function getResults(id) {
+        if(!this.results[id]) this.results[id] = { clicks: 0, exposures: 0 };
+        return this.results[id];
+    };
+
+    Photos.prototype.save = function save() {
+        if(typeof(Storage) !== "undefined") {
+            localStorage.chemistryData = JSON.stringify(this.results);
+        } else {
+            alert('No local storage.  Cannot save data!');
+        }
+    };
+
+    // Load persisted data
+    var photos = new Photos();
+    if(typeof(Storage) !== "undefined") {
+        if(localStorage.chemistryData) {
+            photos.results = JSON.parse(localStorage.chemistryData);
+        }
+    }
+
+    for(var i in photos.data) {
+        photos.data[i].ratio = photos.getRatio(photos.data[i].id);
+    }
+
+    return photos;
+}]);
---
+++ photo-provider.js
@@ -0,0 +1,44 @@
+chemistry.service('PhotoProvider', [function() {
+    return [
+        {
+            id: 5252,
+            name: 'Item A',
+            href: 'http://www.google.com/A',
+            clicks: 0,
+            exposures: 0,
+            ratio: 0,
+            photos: [
+                {
+                    parent: 'A',
+                    name: 'Photo 1',
+                    src: '//placehold.it/500x300&text=A-1'
+                },
+                {
+                    parent: 'A',
+                    name: 'Photo 2',
+                    src: '//placehold.it/500x300&text=A-2'
+                }
+            ]
+        },
+        {
+            id: 233334,
+            name: 'Item B',
+            href: 'http://www.google.com/B',
+            clicks: 0,
+            exposures: 0,
+            ratio: 0,
+            photos: [
+                {
+                    parent: 'B',
+                    name: 'Photo 1',
+                    src: '//placehold.it/500x300&text=B-1'
+                },
+                {
+                    parent: 'B',
+                    name: 'Photo 2',
+                    src: '//placehold.it/500x300&text=B-2'
+                }
+            ]
+        }
+    ]
+}]);
