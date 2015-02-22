17caf7d602f6cbfefa605eb557264dfd8d861cc5
peterbee <peter.bee.parker@gmail.com>
2015-02-22 16:37:08 -0700

    Photo count override for houses

--- houses/photo-provider.js
+++ houses/photo-provider.js
@@ -1,5 +1,13 @@
-chemistry.service('PhotoProvider', ['$http', '$q', function($http, $q) {
+chemistry.service('PhotoProvider', ['$http', '$q', '$location', function($http, $q, $location) {
     function Provider() {};
+
+    var params = {
+        p_limit: 20
+    }
+
+    var userParams = $location.search();
+
+    angular.extend(params, userParams);
 
     Provider.prototype.load = function load() {
         var self = this;
@@ -7,7 +15,7 @@ chemistry.service('PhotoProvider', ['$http', '$q', function($http, $q) {
         if (!self.deferred) {
             self.deferred = $q.defer();
 
-            $http.get('http://riskimo.mooo.com/house-images/50')
+            $http.get('http://riskimo.mooo.com/house-images/'+params.p_limit)
                 .success(function(data, status, headers, config) {
                     self.deferred.resolve(data);
                 })
