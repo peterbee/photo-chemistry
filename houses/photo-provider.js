c048f10fed53a2a7e3d303687e87b106ccfb0ad7
peterbee <peter.bee.parker@gmail.com>
2015-03-15 21:30:32 -0600

    Converted "houses" photo provider to JSON-RPC

--- houses/photo-provider.js
+++ houses/photo-provider.js
@@ -1,27 +1,40 @@
 chemistry.service('PhotoProvider', ['$http', '$q', '$location', function($http, $q, $location) {
+
+    var rpcEndpoint = 'http://dubdub.jakegub.com:10000/rpc';
+
     function Provider() {};
 
-    var params = {
-        p_limit: 20
-    }
-
-    var userParams = $location.search();
-
-    angular.extend(params, userParams);
+    function jsonRpc (method, parameters, config){
+        var data = {"jsonrpc": "2.0", "method": method, "params": parameters, "id" : 1};
+        return $http.post(rpcEndpoint, data, angular.extend({'headers':{'Content-Type': 'application/json'}}, config) );
+    };
 
     Provider.prototype.load = function load() {
         var self = this;
 
+        var params = {
+            p_limit: 20
+        }
+
+        var userParams = $location.search();
+
+        angular.extend(params, userParams);
+
         if (!self.deferred) {
             self.deferred = $q.defer();
 
-            $http.get('http://riskimo.mooo.com/house-images/'+params.p_limit)
-                .success(function(data, status, headers, config) {
-                    self.deferred.resolve(data);
+            var method = 'PhotoChem.GetListings';
+            var parameters = {
+                limit: Number(params.p_limit)
+            };
+
+            jsonRpc(method, parameters)
+                .success(function(data) {
+                    self.deferred.resolve(data.result.listings);
                 })
                 .error(function(data, status, headers, config) {
-                    console.error('Http Error', data);
-                    self.deferred.reject(data);
+                    console.error('JSON RPC Error', data, status, headers, config);
+                    self.deferred.refect(data);
                 });
         }
 
